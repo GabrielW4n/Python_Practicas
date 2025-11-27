652d8dea7a7270614432f9eb99a2f2f0adf7bb93
GabrielW4n <gabrielorozco138@gmail.com>
2025-11-27 14:41:19 -0600

    Create practica1.py

---
+++ practica1.py
@@ -0,0 +1,16 @@
+# Practica 1 - Gabriel
+# Listas, diccionarios y funciones básicas
+
+def saludar(nombre):
+    return f"Hola {nombre}, bienvenido al mundo de Python."
+
+estudiantes = ["Gabriel", "Luis", "Ana"]
+info = {
+    "nombre": "Gabriel",
+    "edad": 18,
+    "carrera": "Inteligencia Artificial"
+}
+
+print(saludar("Gabriel"))
+print("Lista de estudiantes:", estudiantes)
+print("Información:", info)
